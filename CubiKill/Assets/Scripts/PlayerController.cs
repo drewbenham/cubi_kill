fc86ae4212e0b06fcb8f65da535d73f94132722f
drewbenham <drew_benham@yahoo.com>
2020-05-02 09:46:31 -0700

    movement and folder structure
    Added character and settings are set to pixel art. player controller script added.

---
+++ CubiKill/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    Rigidbody2D playerRigidBody;
+
+    public float speed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start() {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // For animation takes in a vector to decide on States.
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        // Get the position of the rigidBody collider
+        Vector2 position = playerRigidBody.position;
+
+        // how the player will move
+        position = position + move * speed * Time.deltaTime;
+
+        playerRigidBody.MovePosition(position);
+    }
+}
